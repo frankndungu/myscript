cb2aa0e9a07328cabe6ae3c65ef280136d972cad
frankndungu <ndungufrank01@gmail.com>
2023-03-12 22:12:01 +0300

    initial commit

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# Echo command
+echo Hello World!
+
+# Variables
+# uppercase by convention
+NAME="Vivian"
+echo "My name is ${NAME}"
+
+# User input
+read -p "Enter your name: " NAME
+echo "Hello $NAME, nice to meet you!"
+
